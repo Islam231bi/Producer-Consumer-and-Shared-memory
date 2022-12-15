bfa6dc0a86b8a23cd1dd7c49f5ad38c20f6c7e69
islam231bi <islambassuni@gmail.com>
2022-12-15 15:40:42 +0200

    added custom library to use for shared memory creation

---
+++ consumer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string>
+#include <random>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int buffer_size = stoi(argv[1]);
+
+    double current_price = 0;
+
+	
+	return 0;
+}
---
+++ shared_memory.h
@@ -0,0 +1,12 @@
+#ifndef SHARED_MEMORY_H
+#define SHARED_MEMORY_H
+
+#include <stdbool.h>
+
+char* attach_memory_block(char* filename, int size);
+bool detach_memory_block(char* block);
+bool destroy_memory_block(char* filename);
+
+#define FILENAME "shared.c"
+
+#endif
